cfb1167fbd5f16c74b3a484a990717fc4d4e31c8
hsuperandy <hsuperandy@gmail.com>
2024-08-29 16:48:17 +0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Navigationbar from './NavigationBar';
+import Header from './Pages/Header';
+import Services from './Pages/Services';
+import Portfolio from './Pages/Portfolio';
+import About from './Pages/About';
+import Team from './Pages/Team';
+import Contact from './Pages/Contact';
+
+function App() {
+  return (
+    <div className="App">
+        <Navigationbar/>
+        <Header/>
+        <Routes>
+          <Route path='/services' element={<Services/>}/>
+          <Route path='/portfolio' element={<Portfolio/>}/> 
+          <Route path='/about' element={<About/>}/>
+          <Route path='/team' element={<Team/>}/>
+          <Route path='/contact' element={<Contact/>}/> 
+        </Routes>
+  
+    </div>
+  );
+}
+
+export default App;
